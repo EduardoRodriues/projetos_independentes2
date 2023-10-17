3cd1343fce43aa08b78da2b75e83ae0fca936e52
Eduardo_Rodrigues_01 <139596440+EduardoRodriues@users.noreply.github.com>
2023-10-17 18:49:33 -0300

    v5

--- Project Complete Responsive Portfolio Website/js/script.js
+++ Project Complete Responsive Portfolio Website/js/script.js
@@ -1,19 +1,33 @@
-/*==================== toggle icon navbar ====================*/
+/*==================== toggles icon navbar ====================*/
 
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-
+menuicon.onclick = () => {
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 /*==================== scroll sections active link ====================*/
 
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-    /*==================== sticky navbar ====================*/
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        };
+    });
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-
-
-
-/*==================== scroll reveal ====================*/
-
-
-/*==================== typed js ====================*/
+    menuicon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
